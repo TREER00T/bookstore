c36c98a4c5e6e5f5a6e862197d317ed2b4aabadb
Ali Azmoodeh <treeroot.ir@gmail.com>
2022-09-05 12:58:04 +0430

    create simple connection for rabbitmq protocol

--- src/api-gateway/src/controller/AuthController.ts
+++ src/api-gateway/src/controller/AuthController.ts
@@ -1,11 +1,32 @@
 import {Request, Response} from 'express';
+import * as amqp from 'amqplib';
+import {config} from 'dotenv';
+import PipeLine from '../middleware/ApiPipeline';
 
+config();
+
+async function connect() {
+    let connection = await amqp.connect(process.env.AMQP_URL);
+    return await connection.createChannel();
+}
 
 export default class AuthController {
 
-    static generateUser(req: Request, res: Response) {
+    static async generateUser(req: Request, res: Response) {
+
+        const CHANNEL_NAME = 'GenerateUser';
+
+        PipeLine.getTokenPayLoad((data: any) => {
+
+            let id = data.id;
+
+            connect().then(channel => {
+                let result = channel.assertQueue(CHANNEL_NAME);
+                channel.sendToQueue(CHANNEL_NAME, Buffer.from(JSON.stringify({data: "", userId: id})));
+            });
 
 
+        });
 
     }
 
